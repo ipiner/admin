8198b1a5ffb0393460f28c6b2517c9df41c73809
ipiner <13717138586@163.com>
2026-08-09 17:30:41 +0800

    trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Exceptions\Handler;
+use Illuminate\Foundation\Configuration\Middleware;
 use Pin\Application;
 
 return Application::configure(dirname(__DIR__))
     ->withExceptions(Handler::class)
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies('*');
+    })
     ->create();
