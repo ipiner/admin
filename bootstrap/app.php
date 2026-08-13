ead3254729ea3f85edbce9d44134f008db736a4e
ipiner <13717138586@163.com>
2026-08-13 20:35:42 +0800

    shouldRenderJsonWhen

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,11 +2,14 @@
 
 use App\Exceptions\Handler;
 use App\Http\Middleware\DemoGuard;
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Pin\Application;
 
 return Application::configure(dirname(__DIR__))
-    ->withExceptions(Handler::class)
+    ->withExceptions(Handler::class, function (Exceptions $exceptions) {
+        $exceptions->shouldRenderJsonWhen(fn () => true);
+    })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->throttleApi('api', true)
             ->trustProxies('*')
